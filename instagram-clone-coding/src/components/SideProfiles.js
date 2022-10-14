9996230dfbd4697a439e0b280a4f97afae38036e
Seoyoung-Mun <tjdud330@gmail.com>
2022-10-14 16:14:46 +0900

    feat : SideProfiles

--- instagram-clone-coding/src/components/SideProfiles.js
+++ instagram-clone-coding/src/components/SideProfiles.js
@@ -10,26 +10,20 @@ import {router} from "react-router-dom";
 function SideProfiles (){
 
     return(
-        <div className="SideProfiles">
-            <Sticky topOffset={-80}>
-                <div className="SideProfiles">
-                    <Profile 
-                        username="Seoyoung_Mun" 
-                        caption="文 瑞 英 (ᴍᴀʀʏ)" 
-                        urlText="전환" 
-                        // 안되면 수정하기
-                        iconSize="big" 
-                        image={image}
-                    /> 
-                    <Suggestions />
-                    <Footer />
-                    {/* <ProfileIcon iconSize="big" image={image} storyBorder={true}/> */}
-
+        <Sticky topOffset={-80}>
+            <div className="SideProfiles">
+                <Profile 
+                    username="Seoyoung_Mun" 
+                    caption="文 瑞 英 (ᴍᴀʀʏ)" 
+                    urlText="전환" 
+                    iconSize="big" 
+                    image={image}
+                /> 
+                <Suggestions />
+                <Footer />
+                {/* <ProfileIcon iconSize="big" image={image} storyBorder={true}/> */}
                 </div>
-            </Sticky>
-            
-            
-        </div>
+        </Sticky>
     );
 }
 
--- instagram-clone-coding/src/components/Suggestions.js
+++ instagram-clone-coding/src/components/Suggestions.js
@@ -1,9 +1,18 @@
 import "../styles/Suggestions.scss";
+import Profile from "./Profile";
 
 function Suggestions () {
     return (
-        <div>
-
+        <div className="Suggestions">
+            <div className="titleContainer">
+                <div className="title">회원님을 위한 추천</div>
+                <a href="/">모두 보기</a>
+            </div>
+            <Profile caption="m0_0n.sy님 외 3명이 팔로우합니다" urlText="팔로우" iconSize="medium" captionSize="small"/>
+            <Profile caption="candy_bar님 외 1명이 팔로우합니다" urlText="팔로우" iconSize="medium" captionSize="small"/>
+            <Profile caption="hoho5050님이 팔로우합니다" urlText="팔로우" iconSize="medium" captionSize="small"/>
+            <Profile caption="Instagram 신규 가입" urlText="팔로우" iconSize="medium" captionSize="small"/>
+            <Profile caption="회원님을 팔로우합니다." urlText="팔로우" iconSize="medium" captionSize="small"/>
         </div>
     )
 }
